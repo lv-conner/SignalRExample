dc3a13c07811647c75c37086dbbc034039122b9f
tim <1510899867@qq.com>
2018-05-15 17:15:02 +0800

    signalr learn

---
+++ SignalRExample/ChatHub.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace SignalRExample
+{
+    [HubName("ChatHub")]
+    public class ChatHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.Hello("tim","SignalR");
+        }
+
+        public void Message(string name,string message)
+        {
+            Clients.All.Message(name, message);
+        }
+
+        public void SendMessageWithID(string name,string message)
+        {
+            Clients.Caller.SendMessageWithID(name, message, Context.ConnectionId);
+        }
+
+        public override Task OnConnected()
+        {
+            return base.OnConnected();
+        }
+
+        public override Task OnReconnected()
+        {
+            return base.OnReconnected();
+        }
+    }
+}
---
+++ SignalRExample/Startup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SignalRExample.Startup))]
+
+namespace SignalRExample
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+            app.MapSignalR<MyConnection>("/myConnection");
+            // 有关如何配置应用程序的详细信息，请访问 https://go.microsoft.com/fwlink/?LinkID=316888
+        }
+    }
+}
